c468d06fe559a3ac1735a4993fbf3dd66bc8784a
SparxCON <izaak.aadi2012@gmail.com>
2026-02-15 17:24:08 +0000

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,7 @@
+
+# To get your keys:
+# 1. GROQ_API_KEY: Sign up at https://console.groq.com/ and create an API key
+# 2. DISCORD_TOKEN: Create a bot at https://discord.com/developers/applications
+
+GROQ_API_KEY = "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"
+DISCORD_TOKEN = "MTQ3MjYzMzg0NTYzODc1ODQ5Mw.GvLN-d.Bi4LnyM84RVVhaIoEcGfXM8gnGo2T7lHeU_f58"
